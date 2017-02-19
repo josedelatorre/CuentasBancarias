3554279cb600745bce171b8d9aed4ccb26866859
triboga <hector@192.168.0.105>
2017-02-19 11:32:30 +0100

    comentarios funciones

--- src/models/Cuenta.java
+++ src/models/Cuenta.java
@@ -80,13 +80,16 @@ public class Cuenta {
 
 	}
 	
-	
+	/**
+	 * genera alias de cuenta aleatoria de 5 letras
+	 * @return alias de cuenta aleatorio
+	 */
 	public static String siguienteAliasCuenta(){
 		
 		char n; 
 		Random rnd = new Random(); 
 		String cadena = new String(); 
-		for (int i=0; i < 10 ; i++) { 
+		for (int i=0; i < 5 ; i++) { 
 		n = (char)(rnd.nextDouble() * 26.0 + 65.0 ); 
 		cadena += n; } 
 		
