4de82cb73c339924a2e31bcbb9a799b6b87c7953
cuent <cuent@DESKTOP-MQT6OIV>
2017-02-20 06:49:20 +0100

    Clases Cuenta y Persona documentadas.

--- src/models/Cuenta.java
+++ src/models/Cuenta.java
@@ -1,34 +1,61 @@
 package models;
 
+import java.awt.TextArea;
+
 public class Cuenta {
 	private String numeroCuenta;
 	private float saldo;
 	
+	/**
+	 * Crea un nuevo objeto de tipo Cuenta con los parámetros especificados.
+	 * @param numeroCuenta el número de cuenta identificativo que tendrá la cuenta.
+	 * @param saldo el saldo inicial con el que contará la cuenta.
+	 */
 	public Cuenta(String numeroCuenta, float saldo) {
 		this.numeroCuenta = numeroCuenta;
 		this.saldo = saldo;
 	}
 
+	/**
+	 * Devuelve el número de cuenta identificativo de la cuenta.
+	 * @return el número de cuenta.
+	 */
 	public String getNumeroCuenta() {
 		return numeroCuenta;
 	}
 
+	/**
+	 * Devuelve el saldo actual de la cuenta.
+	 * @return el saldo actual.
+	 */
 	public float getSaldo() {
 		return saldo;
 	}
 	
+	/**
+	 * Ingresa una cantidad de dinero especificada en la cuenta.
+	 * @param cantidad la cantidad de dinero a ingresar.
+	 */
 	public void recibirAbono(float cantidad){
 		if(cantidad > 0){
 			saldo += cantidad;
 		}
 	}
 	
+	/**
+	 * Retira una cantidad de dinero especificada de la cuenta.
+	 * @param cantidad la cantidad de dinero a retirar.
+	 */
 	public void pagarRecibo(float cantidad){
 		if(cantidad > 0){
 			saldo -= cantidad;
 		}
 	}	
 	
+	/**
+	 * Devuelve un número de cuenta aleatorio convertido a String.
+	 * @return número de cuenta aleatorio, como String.
+	 */
 	public static String siguienteNumeroCuenta(){
 		return String.valueOf(Math.round(Math.random()*10000000));
 	}
--- src/models/Persona.java
+++ src/models/Persona.java
@@ -5,27 +5,39 @@ public class Persona {
 	private String DNI;
 	private Cuenta[] cuentas = new Cuenta[numCuentasMax];
 
+	/**
+	 * Crea un nuevo objeto tipo Persona
+	 * @param dNI El DNI con el que se identificará a la persona.
+	 */
 	public Persona(String dNI) {
 		DNI = dNI;
 	}
 
-	public Persona(String dNI, Cuenta[] cuentas) {
-		DNI = dNI;
-		this.cuentas = cuentas;
-	}
+	//Eliminado segundo constructor
 	
+	/**
+	 * Muestra por consola el estado actual de las cuentas.
+	 */
 	public void imprimirEstadoCuenta(){
 		for (Cuenta cuenta : cuentas) 
 			if(cuenta != null) 
 				System.out.println("Cuenta " + cuenta.getNumeroCuenta() + " saldo: " + cuenta.getSaldo());
 	}
 	
+	/**
+	 * Muestra por consola el estado actual de la cuenta pasada por parámetro.
+	 * @param numCuenta El número de cuenta de la que queremos información.
+	 */
 	public void imprimirEstadoCuenta(int numCuenta){
 		if(cuentas[numCuenta] != null) 
 			System.out.println("Cuenta " + cuentas[numCuenta].getNumeroCuenta() + " saldo: " + cuentas[numCuenta].getSaldo());
 	}
 	
-	
+	/**
+	 * Ingresa una cantidad de dinero en una cuenta bancaria especificada.
+	 * @param cantidad la cantidad de dinero a ingresar.
+	 * @param numCuenta el numero de cuenta destino.
+	 */
 	public void recibirDinero(float cantidad, int numCuenta){
 		if(numCuenta < this.numCuentasMax)
 			cuentas[numCuenta].recibirAbono(cantidad);
@@ -33,6 +45,11 @@ public class Persona {
 			System.out.println("La cuenta número: " + numCuenta + " no existe.");
 	}
 	
+	/**
+	 * Retira una cantidad de dinero en una cuenta bancaria especificada.
+	 * @param cantidad cantidad de dinero a retirar.
+	 * @param numCuenta el numero de cuenta afectada.
+	 */
 	public void pagarDinero(float cantidad, int numCuenta){
 		if(numCuenta < this.numCuentasMax)
 			cuentas[numCuenta].pagarRecibo(cantidad);
@@ -40,6 +57,12 @@ public class Persona {
 			System.out.println("La cuenta número: " + numCuenta + " no existe.");
 	}
 	
+	/**
+	 * Realiza un traspaso de dinero entre la primera cuenta especificada y la segunda.
+	 * @param numCuenta1 la cuenta de la que se quiere extraer el dinero.
+	 * @param numCuenta2 la cuenta en la que se quiere recibir el dinero.
+	 * @param cantidad la cantidad de dinero que se quiere traspasar.
+	 */
 	public void realizarTransferencia(int numCuenta1, int numCuenta2, float cantidad){
 		pagarDinero(cantidad, numCuenta1);
 		recibirDinero(cantidad, numCuenta2);
@@ -62,10 +85,17 @@ public class Persona {
 		return esMorosa;
 	}
 
+	/**
+	 * Crea una nueva cuenta sin ingreso inicial.
+	 */
 	public void nuevaCuenta() {
 		this.nuevaCuenta(0);
 	}
 	
+	/**
+	 * Crea una nueva cuenta con un ingreso inicial especificado.
+	 * @param ingresoInicial la cantidad de dinero a ingresar.
+	 */
 	public void nuevaCuenta(float ingresoInicial) {
 		String numeroCuenta = Cuenta.siguienteNumeroCuenta();
 		int numeroCuentas = this.numeroCuentas();
@@ -80,7 +110,6 @@ public class Persona {
 
 	/**
 	 * Calcula el numero de cuentas creadas de la persona
-	 * 
 	 * @return Numero de cuentas creadas.
 	 */
 	private int numeroCuentas() {
